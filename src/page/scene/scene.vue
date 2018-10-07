c29d16ed638b76e20164ffabd81a4b278dca0eb0
lovemo <lovemomoyulin@qq.com>
2018-10-07 21:14:14 +0800

    add tree

--- src/page/scene/scene.vue
+++ src/page/scene/scene.vue
@@ -1,23 +1,61 @@
 <template>
     <div class="scene-container">
         <div class="title"><span>scene</span></div>
-     
+        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
     </div>
 </template>
 
 <script>
-    
-
-    export default {
-        data() {
-            return {
-
-            }
-        },
-        methods: {
-           
+  export default {
+    data() {
+      return {
+        data: [{
+          label: '一级 1',
+          children: [{
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+          children: [{
+            label: '二级 3-1',
+            children: [{
+              label: '三级 3-1-1'
+            }]
+          }, {
+            label: '二级 3-2',
+            children: [{
+              label: '三级 3-2-1'
+            }]
+          }]
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
         }
+      };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      }
     }
+  };
 </script>
 
 <style scoped>
